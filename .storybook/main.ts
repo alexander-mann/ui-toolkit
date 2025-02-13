63b9caac0390844f6ebcbd26534c1c238a00d4d9
alexander-mann <alexandercmann@gmail.com>
2025-02-13 14:58:16 -0500

    fix: storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,10 +5,8 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-webpack5-compiler-swc',
-    '@storybook/addon-onboarding',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
     '@storybook/addon-interactions',
     '@storybook/addon-styling-webpack',
     '@storybook/addon-themes',
